15a61a19a06c5abc1537c323409f0bde223c75f8
Bollsjen <magnusjensen999@gmail.com>
2022-09-02 01:59:23 +0200

    test

---
+++ _secrets/SecretStuff.php
@@ -0,0 +1,9 @@
+<?php
+
+    function GoogleAPIKey() : string{
+        return "AIzaSyD1nHBTBTkJtRrWmpJb2ZxihkGiyG5FQO0";
+    }
+
+    function RecapchaSecret() : string{
+        return "6LeZ6mkgAAAAAAJqDlLg044zX9iDmY82C827P1Ve";
+    }
