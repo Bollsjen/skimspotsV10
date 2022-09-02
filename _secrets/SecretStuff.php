3472fe842278ad2af9a22f4b15522ef55086aa91
Bollsjen <55839056+Bollsjen@users.noreply.github.com>
2022-09-02 02:44:39 +0200

    fuck af

--- _secrets/SecretStuff.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    function GoogleAPIKey() : string{
-        return "AIzaSyD1nHBTBTkJtRrWmpJb2ZxihkGiyG5FQO0";
-    }
-
-    function RecapchaSecret() : string{
-        return "6LeZ6mkgAAAAAAJqDlLg044zX9iDmY82C827P1Ve";
-    }
